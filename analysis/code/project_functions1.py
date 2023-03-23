1534dba92d56ba055a14aa4338e6716b551776c6
Aticha Amie Prasongsukarn <amieaticha2001@gmail.com>
2023-03-23 11:19:11 -0700

    Completed milestone4

---
+++ analysis/code/project_functions1.py
@@ -0,0 +1,14 @@
+def load_and_process(url_or_path_to_csv_file):
+    df1 = (
+          pd.read_csv(url_or_path_to_csv_file)
+          .drop(['sex', 'bmi','children', 'smoker', 'region'], axis = 1)
+          .dropna(axis = 0)
+      )
+    return df1
+
+def clean_and_wrangle(df):
+    df1 = (
+          df.groupby(['age'],as_index=False)['charges'].mean()
+            .rename(columns = {'age': 'Age','charges': 'Average cost'})
+      )
+    return df1
